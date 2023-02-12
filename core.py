af69259351e112179613bbdb37fbc96eac252ec2
tlucanti <prototype3628800@yandex.ru>
2023-02-12 21:16:48 +0300

    refractored, fixed clone

--- core.py
+++ core.py
@@ -3,8 +3,8 @@ from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 import math
-import os as _Os_module
-import time as _Time_module
+import os as _os_module
+import time
 from queue import Queue
 import threading
 
@@ -58,7 +58,7 @@ class GameWindow(QGraphicsView):
         rpc_call = self.rpc_queue.get()
         rpc_call.call(*rpc_call.args)
         s = self.rpc_queue.qsize()
-        if s != 0:
+        if s > 10:
             print('queue size', self.rpc_queue.qsize())
 
     def mouseMoveEvent(self, event):
@@ -67,50 +67,47 @@ class GameWindow(QGraphicsView):
 
 class GameBase():
     def __init__(self, resx, resy, title):
-        self.__app = QApplication([])
-        self.__window = GameWindow(resx, resy, title)
-        self.__window.show()
+        self.app = QApplication([])
+        self.window = GameWindow(resx, resy, title)
+        self.window.show()
 
-    def __method_mouse(self):
-        return Pair(self.__window.mouse_pos.x(), self.__window.mouse_pos.y())
+    def mouse(self):
+        return Pair(int(self.window.mouse_pos.x()), int(self.window.mouse_pos.y()))
 
-    def __method_loop(self):
-        self.__app.exec()
+    def mouse_x(self):
+        return int(self.window.mouse_pos.x())
 
-    def __method_mouse_x(self):
-        return self.__window.mouse_pos.x()
-
-    def __method_mouse_y(self):
-        return self.__window.mouse_pos.y()
+    def mouse_y(self):
+        return int(self.window.mouse_pos.y())
 
 
-class Game(GameBase):
+class Game():
     def __init__(self, resx=1000, resy=1000, title='scratch game'):
-        super().__init__(resx, resy, title)
+        self.__internal_obj_ref = GameBase(resx, resy, title)
 
     def mouse(self):
         """
         returns mouse object
         """
-        return self._GameBase__method_mouse()
+        return self.__internal_obj_ref.mouse()
 
     def mouse_x(self):
         """
         returns mouse (x) coordinate
         """
-        return self._GameBase__method_mouse_x()
+        return self.__internal_obj_ref.mouse_x()
 
     def mouse_y(self):
         """
         returns mouse (y) coordianate
         """
-        return self._GameBase__method_mouse_y()
+        return self.__internal_obj_ref.mouse_y()
 
     def loop(self):
         """
         start game main loop
         """
-        return self._GameBase__method_loop()
+        return self.__internal_obj_ref.app.exec()
 
 
 class RPCcall():
@@ -121,10 +118,11 @@ class RPCcall():
 
 class Worker(QThread):
     def __init__(self, obj, pixmap, routine, args, kwargs):
-        self.x = 0
-        self.y = 0
+        self.pos_x = 0
+        self.pos_y = 0
         self.angle = 0
-        self.rpc_queue = args[0]._GameBase__window.rpc_queue
+        game = args[0]
+        self.rpc_queue = game._Game__internal_obj_ref.window.rpc_queue
         self.obj = obj
 
         self.orig = pixmap
@@ -134,34 +132,37 @@ class Worker(QThread):
         self.kwargs = kwargs
         super().__init__()
 
+    def __del__(self):
+        self.wait()
+
     def rpc_set_pos(self, x, y):
         self.rpc_queue.put(RPCcall(self.obj.setPos, x, y))
 
     def rpc_set_rotation(self, angle):
         self.rpc_queue.put(RPCcall(self. obj.setRotation, angle))
 
-    def setpos(self, x, y):
-        self.x = x
-        self.y = y
+    def set_pos(self, x, y):
+        self.pos_x = x
+        self.pos_y = y
         self.rpc_set_pos(x - self.pixmap.width() // 2,
-                        y - self.pixmap.height() // 2)
+                         y - self.pixmap.height() // 2)
 
     def move(self, x, y):
-        self.setpos(self.x + x, self.y + y)
+        self.setpos(self.pos_x + x, self.pos_y + y)
 
-    def setrotation(self, angle):
+    def set_rotation(self, angle):
         self.angle = angle
         self.rpc_set_rotation(-angle)
 
-    def right(self, angle):
-        self.setrotation(self.angle + angle)
+    def turn_right(self, angle):
+        self.set_rotation(self.angle + angle)
 
-    def left(self, angle):
-        self.setrotation(self.angle - angle)
+    def turn_left(self, angle):
+        self.set_rotation(self.angle - angle)
 
     def point_to(self, obj):
-        angle = math.atan2(obj.get_x() - self.y, obj.get_y() - self.x)
-        self.setrotation(round(angle * 180 / math.pi) - 90)
+        angle = math.atan2(obj.get_x() - self.pos_y, obj.get_y() - self.pos_x)
+        self.set_rotation(round(angle * 180 / math.pi) - 90)
 
     def run(self):
         self.routine(*self.args, **self.kwargs)
@@ -169,117 +170,122 @@ class Worker(QThread):
 
 class SpriteBase(QWidget):
 
-    def __init__(self, path, args, kwargs, is_clone=False):
+    def __init__(self, routine, path, worker_args, worker_kwargs):
         super().__init__()
-        if not _Os_module.path.exists(path):
+        if not _os_module.path.exists(path):
             raise ValueError(f'path ({path}) does not exist')
-        self.__game = args[0]
-        self.__scene = self.__game._GameBase__window.scene
 
         pixmap = QPixmap(path)
-        self.__obj = self.__scene.addPixmap(pixmap)
-        center = self.__obj.boundingRect().center()
-        self.__obj.setTransformOriginPoint(center)
-        if is_clone:
-            routine = self.as_clone
-        else:
-            routine = self.run
+        game = worker_args[0]
+        scene = game._Game__internal_obj_ref.window.scene
+        self.obj = scene.addPixmap(pixmap)
+        center = self.obj.boundingRect().center()
+        self.obj.setTransformOriginPoint(center)
 
-        conn_type = Qt.ConnectionType.BlockingQueuedConnection
-        self.__path = path
-        self.__worker = Worker(self.__obj, pixmap, routine, args, kwargs)
-        self.__worker.start()
+        self.path = path
+        self.worker = Worker(self.obj, pixmap, routine, worker_args, worker_kwargs)
 
     def __str__(self):
         n = type(self).__name__
-        x = self.__worker.x
-        y = self.__worker.y
+        x = self.worker.pos_x
+        y = self.worker.pos_y
         return f'{n}(x={x}, y={y})'
 
-    def __internal_delay(self):
-        _Time_module.sleep(1 / 60)
-
-    def __method_setpos(self, x, y):
-        self.__worker.setpos(x, y)
-        self.__internal_delay()
-
-    def __method_setrotation(self, angle):
-        self.__worker.setrotation(angle)
-        self.__internal_delay()
-
-    def __method_right(self, angle):
-        self.__worker.right(angle)
-        self.__internal_delay()
-
-    def __method_left(self, angle):
-        self.__worker.left(angle)
-        self.__internal_delay()
-
-    def __method_point_to(self, obj):
-        self.__worker.point_to(obj)
-        self.__internal_delay()
-
-    def __method_get_x(self):
-        return self.__worker.x
-
-    def __method_get_y(self):
-        return self.__worker.y
-
-    def __method_clone(self, args, kwargs):
+    def clone(self, parent, clone_args, clone_kwargs):
         #print(sprite(self.__path, True)(type(self))(*args))
-        clone = type(self).__new__(type(self))
-        SpriteBase.__init__(clone, self.__path, args, kwargs, is_clone=True)
+        if len(clone_args) == 0 or type(clone_args[0]) != Game:
+            raise TypeError('expected (Game) for first argument of clone method')
 
-    def __method_sleep(self, period):
-        pass
-        #_Time_module.sleep(period)
+        cloned_parent = type(parent).__new__(type(parent))
+        Sprite.__init__(cloned_parent, cloned_parent.as_clone, self.path, clone_args, clone_kwargs)
+        return cloned_parent
 
 
-class Sprite(SpriteBase):
-    def __init__(self, *args):
-        super().__init__(*args)
+def sprite(path):
+    def sprite_wrapper(original_class):
+        def new_init(self, *args, **kwargs):
+            if len(args) == 0 or type(args[0]) != Game:
+                raise TypeError('expected (Game) for first argument')
+            Sprite.__init__(self, self.run, path, args, kwargs)
 
-    def setpos(self, x, y):
+        original_class.__init__ = new_init
+        return original_class
+    return sprite_wrapper
+
+def rpc_method(function):
+    def wrapper(*args, **kwargs):
+        retval = function(*args, **kwargs)
+        time.sleep(0.01)
+        return retval
+    return wrapper
+
+class Sprite():
+    def __init__(self, routine, image_path, worker_args, worker_kwargs):
+        self.__internal_obj_ref = SpriteBase(routine, image_path, worker_args, worker_kwargs)
+        self.__internal_obj_ref.worker.start()
+
+    def __str__(self):
+        return self.__internal_obj_ref.__str__()
+
+    @rpc_method
+    def set_pos(self, x, y):
         """
         set sprite position to (x, y)
         """
-        self._SpriteBase__method_setpos(x, y)
+        self.__internal_obj_ref.worker.set_pos(x, y)
 
-    def setrotation(self, angle):
+    @rpc_method
+    def move(self, x, y):
+        """
+        move to (x, y) shift from current position
+        """
+        self.__internal_obj_ref.worker.move(x, y)
+
+    @rpc_method
+    def set_rotation(self, angle):
         """
         set sprite rotation to (angle) degrees
         """
-        self._SpriteBase__method_setrotation(angle)
+        self.__internal_obj_ref.worker.set_rotation(angle)
 
-    def right(self, angle):
+    @rpc_method
+    def turn_right(self, angle):
         """
         rotate sprite to (angle) degrees clockwise
         """
-        self._SpriteBase__method_right(angle)
+        self.__internal_obj_ref.worker.turn_right(angle)
 
-    def left(self, angle):
+    @rpc_method
+    def turn_left(self, angle):
         """
         rotate sprite to (angle) degrees counter clockwise
         """
-        self._SpriteBase__method_left(angle)
+        self.__internal_obj_ref.worker.turn_left(angle)
 
+    @rpc_method
     def point_to(self, object):
         """
         set sprite rotation to point to (object)
         """
-        self._SpriteBase__method_point_to(object)
+        self.__internal_obj_ref.worker.point_to(object)
 
     def get_x(self):
         """
         returns sprites (x) coordinate
         """
-        return self._SpriteBase__method_get_x()
+        return self.__internal_obj_ref.worker.pos_x
 
     def get_y(self):
         """
         returns sprites (y) coordinate
         """
-        return self._SpriteBase__method_get_y()
+        return self.__internal_obj_ref.worker.pos_y
+
+    def get_angle(self):
+        """
+        return (angle) value in degrees
+        """
+        return self.__internal_obj_ref.worker.angle
 
     def run(self):
         """
@@ -287,11 +293,12 @@ class Sprite(SpriteBase):
         """
         pass
 
+    @rpc_method
     def clone(self, *args, **kwargs):
         """
         create and return sprite clone
         """
-        return self._SpriteBase__method_clone(args, kwargs)
+        return self.__internal_obj_ref.clone(self, args, kwargs)
 
     def as_clone(self, *args, **kwargs):
         """
@@ -299,26 +306,18 @@ class Sprite(SpriteBase):
         """
         pass
 
-    def sleep(self, interval):
-        self._SpriteBase__method_sleep(interval)
-
-
-def sprite(path, __clone=False):
-    def sprite_wrapper(original_class):
-        def new_init(self, *args, **kwargs):
-            if len(args) == 0 or type(args[0]) != Game:
-                raise TypeError('expected (Game) for first argument')
-            Sprite.__init__(self, path, args, kwargs, __clone)
-
-        original_class.__init__ = new_init
-        return original_class
-    return sprite_wrapper
-
 
 @sprite('images/sprite.png')
 class Box1(Sprite):
     def run(self, game):
-        self.setpos(200, 200)
+        self.set_pos(200, 200)
+        clone = self.clone(game)
+        while True:
+            self.point_to(game.mouse())
+
+    def as_clone(self, game):
+        print('b1 clone')
+        self.set_pos(800, 800)
         while True:
             self.point_to(b2)
 
@@ -326,13 +325,15 @@ class Box1(Sprite):
 @sprite('images/sprite2.png')
 class Box2(Sprite):
     def run(self, game):
-
-        c = self.clone(game, 100, 100)
+        c = self.clone(game)
+        print(c)
+        c.set_pos(100, 100)
+        self.set_pos(200, 200)
         while True:
-            self.setpos(game.mouse_x(), game.mouse_y())
+            self.set_pos(game.mouse_x(), game.mouse_y())
 
-    def as_clone(self, game, x, y):
-        self.setpos(x, y)
+    def as_clone(self, game):
+        print('b2 clone')
 
 
 if __name__ == '__main__':
@@ -340,6 +341,7 @@ if __name__ == '__main__':
 
     b2 = Box2(g)
     b1 = Box1(g)
+
 
     g.loop()
 
